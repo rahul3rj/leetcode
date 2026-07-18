40b8c58d5a77efa3e23e1aa828bd0b884a64f9ea
Rahul Jha <147395419+rahul3rj@users.noreply.github.com>
2026-07-18 14:04:36 +0530

    [678] [Time Beats: 100.00%] [Memory Beats: 19.81%] - LeetPush

---
+++ 678-valid-parenthesis-string/678-Valid-Parenthesis-String.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int max  = 0;
+        int min = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '('){
+                max++;
+                min++;
+            }else if(s.charAt(i) == ')'){
+                max--;
+                min--;
+            }else{
+                min--;
+                max++;
+            }
+            if(min < 0){
+                min = 0;
+            }
+            if(max < 0) return false;
+        }
+        return (min == 0);
+    }
+}
