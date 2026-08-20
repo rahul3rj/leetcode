af9e22488bec3d2f96121b51e2af492079ae5740
Rahul Jha <147395419+rahul3rj@users.noreply.github.com>
2026-08-20 10:49:17 +0530

    [236] [Time Beats: 52.28%] [Memory Beats: 61.36%] - LeetPush

---
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-Lowest-Common-Ancestor-of-a-Binary-Tree.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null) return null;
+        if(root == p || root == q) return root;
+        TreeNode leftC = lowestCommonAncestor(root.left, p, q);
+        TreeNode rightC = lowestCommonAncestor(root.right, p, q);
+        if(leftC == null){
+            return rightC;
+        }
+        else if(rightC == null){
+            return leftC;
+        }else{
+            return root;
+        }
+    }
+}
