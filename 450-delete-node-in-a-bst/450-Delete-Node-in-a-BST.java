72727e6d39ed6ee570f2cf805433e3d02e2cf144
Rahul Jha <147395419+rahul3rj@users.noreply.github.com>
2026-08-30 12:20:06 +0530

    [450] [Time Beats: 100.00%] [Memory Beats: 95.31%] - LeetPush

---
+++ 450-delete-node-in-a-bst/450-Delete-Node-in-a-BST.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root == null) return null;
+        if(root.val == key) return helper(root);
+        TreeNode dup = root;
+        while(root != null){
+            if(root.val > key){
+                if(root.left != null && root.left.val == key){
+                    root.left = helper(root.left);
+                    break;
+                }else{
+                    root = root.left;
+                }
+            }else{
+                if(root.right != null && root.right.val == key){
+                    root.right = helper(root.right);
+                    break;
+                }else{
+                    root = root.right;
+                }
+            }
+        }
+        return dup;
+    }
+    private TreeNode helper(TreeNode root){
+        if(root.left == null) return root.right;
+        else if(root.right == null) return root.left;
+        TreeNode rightC = root.right;
+        TreeNode lastRight = findlastR(root.left);
+        lastRight.right = rightC;
+        return root.left;
+    }
+    private TreeNode findlastR(TreeNode root){
+        if(root.right == null) return root;
+        return findlastR(root.right);
+    }
+}
